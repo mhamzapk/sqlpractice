45a933df216a503f6e0097b19db05dd8c7c8e8cd
mhamzapk <77717285+mhamzapk@users.noreply.github.com>
2021-01-27 07:58:20 +0530

    Update hackerrank1.sql

--- hackerrank1.sql
+++ hackerrank1.sql
@@ -1,5 +1,10 @@
+//question1
 select distinct city from station where (CITY LIKE '%a' 
     OR CITY LIKE '%e' 
     OR CITY LIKE '%i' 
     OR CITY LIKE '%o'
     OR CITY LIKE '%u');
+    
+  //question2 = Weather Observation Station 11
+  
+  select distinct city from station where (city not like 'A%' and city not like 'E%' and city not like 'I%' and city not like 'O%' and city not like 'U%');
