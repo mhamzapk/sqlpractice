78603b4d675ea8d8c00a7083f73b3b2d20f5b355
mhamzapk <77717285+mhamzapk@users.noreply.github.com>
2021-01-27 08:11:23 +0530

    Create sqlhackerques3.sql

---
+++ sqlhackerques3.sql
@@ -0,0 +1,3 @@
+// question 3 = Weather Observation Station 10
+
+select distinct city from station where (city not like '%a' and city not like '%e' and city not like '%i' and city not like '%o' and city not like '%u');
